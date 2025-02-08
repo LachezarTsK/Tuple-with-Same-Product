590dced9f20e19abfc5fab0c7189e2fe01403ba3
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-08 11:00:42 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,16 @@
+
+function tupleSameProduct(input: number[]): number {
+    let tupleCombinationsOfSameProduct = 0;
+    const valueToNumberOfPairs = new Map<number, number>();
+
+    for (let i = 0; i < input.length; ++i) {
+        for (let j = i + 1; j < input.length; ++j) {
+            const product = input[i] * input[j];
+            const numberOfPairs = valueToNumberOfPairs.has(product) ? valueToNumberOfPairs.get(product) : 0;
+
+            tupleCombinationsOfSameProduct += numberOfPairs;
+            valueToNumberOfPairs.set(product, numberOfPairs + 1);
+        }
+    }
+    return tupleCombinationsOfSameProduct * 8;
+};
