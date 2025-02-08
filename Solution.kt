379727d626d338fcbf9e1490f7ad40a7222bcc08
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-08 11:02:27 +0100

    Create Solution.kt

---
+++ Solution.kt
@@ -0,0 +1,19 @@
+
+class Solution {
+
+    fun tupleSameProduct(input: IntArray): Int {
+        var tupleCombinationsOfSameProduct = 0
+        val valueToNumberOfPairs = HashMap<Int, Int>()
+
+        for (i in input.indices) {
+            for (j in i + 1..<input.size) {
+                val product = input[i] * input[j]
+                val numberOfPairs = valueToNumberOfPairs.getOrDefault(product, 0)
+
+                tupleCombinationsOfSameProduct += numberOfPairs
+                (valueToNumberOfPairs[product]) = numberOfPairs + 1
+            }
+        }
+        return tupleCombinationsOfSameProduct * 8
+    }
+}
